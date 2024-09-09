d8bc35560baace487c34288f49087b2b43a70245
Sergio Pecan <u20201a938@upc.edu.pe>
2024-09-09 08:38:02 -0500

    update

--- src/main/java/com/paway/spring/data/kmoneta/config/OpenAPIConfig.java
+++ src/main/java/com/paway/spring/data/kmoneta/config/OpenAPIConfig.java
@@ -2,21 +2,15 @@ package com.paway.spring.data.kmoneta.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+
 
 @Configuration
 public class OpenAPIConfig {
-
-
-
   @Bean
   public OpenAPI myOpenAPI() {
     // General configuration
