9d065672c04cd7232d0b198323aa175bc41c2002
Sergio Pecan <u20201a938@upc.edu.pe>
2024-09-09 19:50:44 -0500

    feat(update): kmoneta

---
+++ src/main/java/com/paway/spring/data/kmoneta/supplier/repository/ProviderRepository.java
@@ -0,0 +1,17 @@
+package com.paway.spring.data.kmoneta.supplier.repository;
+
+import com.paway.spring.data.kmoneta.supplier.model.Provider;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+import java.util.List;
+public interface ProviderRepository extends MongoRepository<Provider, String> {
+
+    List<Provider> findByName(String name);
+
+
+    List<Provider> findByAddress_City(String city);
+
+
+    List<Provider> findByAddress_State(String state);
+}
