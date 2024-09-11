aec59999ec8d019d8ff8f446f0708f50b4a48b7e
André <u202217772@upc.edu.pe>
2024-09-11 17:24:41 -0500

    Update

---
+++ src/main/java/com/paway/spring/data/kmoneta/model/Product.java
@@ -0,0 +1,69 @@
+package com.paway.spring.data.kmoneta.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String id;
+    private String description;
+    private int price;
+    private String productName;
+    private int stock;
+
+    public Product() {
+    }
+
+    public Product(String description, int price, String productName, int stock) {
+        this.description = description;
+        this.price = price;
+        this.productName = productName;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", description=" + description + ", price=" + price + ", productName=" + productName + ", stock=" + stock + "]";
+    }
+}
