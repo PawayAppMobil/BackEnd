b999c36b9216c838740c0ddbd6c4912e4d87933b
Arturo Adrianzén <cartureto@gmail.com>
2024-09-23 15:29:14 -0500

    feat(reports): add GET endpoint for expense category

--- src/main/java/com/paway/spring/data/kmoneta/reports/controller/ExpenseCategoryController.java
+++ src/main/java/com/paway/spring/data/kmoneta/reports/controller/ExpenseCategoryController.java
@@ -4,10 +4,9 @@ import com.paway.spring.data.kmoneta.reports.model.ExpenseCategory;
 import com.paway.spring.data.kmoneta.reports.repository.ExpenseCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expense-categories")
@@ -20,4 +19,19 @@ public class ExpenseCategoryController {
         ExpenseCategory nuevaCategoria = expenseCategoryRepository.save(expenseCategory);
         return ResponseEntity.ok(nuevaCategoria);
     }
+
+    @GetMapping
+    public ResponseEntity<List<ExpenseCategory>> obtenerTodasLasCategorias() {
+        List<ExpenseCategory> categorias = expenseCategoryRepository.findAll();
+        return ResponseEntity.ok(categorias);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpenseCategory> obtenerCategoriaPorId(@PathVariable String id) {
+        return expenseCategoryRepository.findById(id)
+                .map(categoria -> ResponseEntity.ok(categoria))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
