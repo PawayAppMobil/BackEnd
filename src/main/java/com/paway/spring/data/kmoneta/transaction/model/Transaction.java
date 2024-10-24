5c2b38f6fade32e83f38dcb360f12897d98e3fac
Arturo Adrianzén <cartureto@gmail.com>
2024-10-24 14:54:25 -0500

    fix (transactions): added validations to transactions variables: amount, date, details, isIncome, userId

--- src/main/java/com/paway/spring/data/kmoneta/transaction/model/Transaction.java
+++ src/main/java/com/paway/spring/data/kmoneta/transaction/model/Transaction.java
@@ -1,6 +1,10 @@
 package com.paway.spring.data.kmoneta.transaction.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -15,13 +19,25 @@ public class Transaction {
 
     @Id
     private String id;
+
+    @NotNull(message = "El monto no puede ser nulo.")
+    @Min(value = 0, message = "El monto no puede ser negativo.")
     private double amount;
+
+    @NotNull(message = "La fecha no puede ser nula.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
+
+    @NotBlank(message = "Los detalles no pueden estar vacíos.")
+    @Size(max = 255, message = "Los detalles no pueden exceder los 255 caracteres.")
     private String details;
+
     private String invoiceId;
 
+    @NotNull(message = "El campo 'isIncome' no puede ser nulo.")
     private boolean isIncome;
+
+    @NotBlank(message = "El userId no puede estar vacío.")
     private String userId;
 
     public Transaction() {
